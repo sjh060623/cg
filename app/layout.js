d72ba0d0811db34f7ce38e31f3fcd4add8b80398
손정훈 <sonjeonghoon@sonjeonghuns-MacBook-Air.local>
2023-11-25 00:23:36 +0900

    ad.3

--- app/layout.js
+++ app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
           />
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3852248362281327"
      crossorigin="anonymous"></script>
+     <meta name="google-adsense-account" content="ca-pub-3852248362281327"></meta>
         </Head>
         
         <Header />
